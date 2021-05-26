d7b4f714bff6e5b29a919131a5a31424193d9075
布目　喜裕 <bumuxiyu@nunomeyoshihironoMacBook-Pro.local>
2021-05-26 21:29:37 +0900

    try only cuda

--- 16_final_report/cuda_only.cu
+++ 16_final_report/cuda_only.cu
@@ -10,7 +10,7 @@ __global__ void matrix(float *a,float *b,float *c,int N, int offset,int size){
   if (l <N){
     for (int i=0; i<N/size; i++)
       for (int j=0; j<N/size; j++)
-           c[N*i+j+offset] += subA[N*i+l]*subB[N/size*l+j]
+           c[N*i+j+offset] += a[N*i+l]*b[N/size*l+j]
       //subC[N*i+j+offset] += subA[N*i+k] * subB[N/size*k+j];
 
   }
