4eeaa5ae8b8b82c643e91e8b99bab073b5720f17
布目　喜裕 <bumuxiyu@nunomeyoshihironoMacBook-Pro.local>
2021-05-27 09:15:57 +0900

    add:cudaDeviceSynchronize();

--- 16_final_report/cuda.cu
+++ 16_final_report/cuda.cu
@@ -84,6 +84,7 @@ int main(int argc, char** argv) {
     offset = N/size*((rank+irank) % size);
 
     matrix<<<(N/size+M-1)/M,M>>>(a,b,c,N,offset,size);
+    cudaDeviceSynchronize();
 
     auto toc = chrono::steady_clock::now();
     comp_time += chrono::duration<double>(toc - tic).count();
