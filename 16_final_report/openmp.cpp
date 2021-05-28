6d7b1bedf32afc984fcc3eb814d0067927d007d2
布目　喜裕 <bumuxiyu@nunomeyoshihironoMacBook-Pro.local>
2021-05-28 23:24:42 +0900

    OpenMP & MPI

--- 16_final_report/openmp.cpp
+++ 16_final_report/openmp.cpp
@@ -56,8 +56,9 @@ int main(int argc, char** argv) {
     tic = chrono::steady_clock::now();
     comm_time += chrono::duration<double>(tic - toc).count();
   }
-  MPI_Barrier(MPI_COMM_WORLD);
   MPI_Allgather(&subC[0], N*N/size, MPI_FLOAT, &C[0], N*N/size, MPI_FLOAT, MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+
   for (int i=0; i<N; i++)
     for (int j=0; j<N; j++)
       for (int k=0; k<N; k++)
