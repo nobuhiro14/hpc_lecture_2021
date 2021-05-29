b672340e6812eb95c7dfad14cb6ddecb23132d2e
布目　喜裕 <bumuxiyu@nunomeyoshihironoMacBook-Pro.local>
2021-05-29 21:30:50 +0900

    debug

--- 16_final_report/cuda_only.cu
+++ 16_final_report/cuda_only.cu
@@ -63,54 +63,14 @@ int main(int argc, char** argv) {
 
   for (int i=0; i<N/size; i++)
     for (int j=0; j<N; j++)
-      subA[N*i+j] = A[N*(i+offset)+j];
+      subA[N*i+j] = A[N*(i+0)+j];
   for (int i=0; i<N; i++)
     for (int j=0; j<N/size; j++)
       subB[N/size*i+j] = B[N*i+j+offset];
   cudaMemcpy(subA,a,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
   cudaMemcpy(subB,b,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
 
-  double comp_time = 0, comm_time = 0;
-
-    auto tic = chrono::steady_clock::now();
-    /*
-    offset = N/size*((rank+irank) % size);
-    for (int i=0; i<N/size; i++)
-      for (int j=0; j<N/size; j++)
-        for (int k=0; k<N; k++)
-          subC[N*i+j+offset] += subA[N*i+k] * subB[N/size*k+j];
-    */
-    offset = N/size*((rank) % size);
-    printf("before matrix\n");
-
-    //matrix<<<(N+M-1)/M,M>>>(a,b,c,N,offset,size);
-    //cudaDeviceSynchronize();
-    printf("after matrix\n");
-
-
-    auto toc = chrono::steady_clock::now();
-    comp_time += chrono::duration<double>(toc - tic).count();
-    //MPI_Barrier(MPI_COMM_WORLD);
-    printf("after comm\n");
-    //cudaMemcpy(b,subB,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
-    tic = chrono::steady_clock::now();
-    comm_time += chrono::duration<double>(tic - toc).count();
-
-  //cudaMemcpy(subC,c,N*N/size*sizeof(float),cudaMemcpyDeviceToHost);
-
-
-  double err = 0;
-
-  if(rank==0) {
-    double time = comp_time+comm_time;
-    printf("A[10]: %lf\n",A[10]);
-    printf("N    : %d\n",N);
-    printf("size : %d\n",size);
-    printf("comp : %lf s\n", comp_time);
-    printf("comm : %lf s\n", comm_time);
-    printf("total: %lf s (%lf GFlops)\n",time,2.*N*N*N/time/1e9);
-    printf("error: %lf\n",err/N/N);
-  }
+  
   free(subA);
   free(subB);
   free(subC);
