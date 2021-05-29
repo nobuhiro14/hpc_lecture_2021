648df2e1a96007591bd357e9e442c4acd42e5c9b
布目　喜裕 <bumuxiyu@nunomeyoshihironoMacBook-Pro.local>
2021-05-29 21:38:25 +0900

    debug

--- 16_final_report/cuda_only.cu
+++ 16_final_report/cuda_only.cu
@@ -29,14 +29,17 @@ int main(int argc, char** argv) {
   vector<float> B(N*N);
   vector<float> C(N*N, 0);
   float *subA, *subB, *subC,*recv;
-
+  subA = (float *)malloc(N*N*sizeof(float));
+  subB = (float *)malloc(N*N*sizeof(float));
+  subC = (float *)malloc(N*N*sizeof(float));
+  recv = (float *)malloc(N*N*sizeof(float));
 
   float *a;
   float *b;
   float *c;
-  cudaMalloc(&a, N*N/size*sizeof(float));
-  cudaMalloc(&b, N*N/size*sizeof(float));
-  cudaMalloc(&c, N*N/size*sizeof(float));
+  cudaMalloc(&a, N*N*sizeof(float));
+  cudaMalloc(&b, N*N*sizeof(float));
+  cudaMalloc(&c, N*N*sizeof(float));
 
   for (int i=0; i<N; i++) {
     for (int j=0; j<N; j++) {
@@ -54,13 +57,22 @@ int main(int argc, char** argv) {
     for (int j=0; j<N/size; j++)
       subB[N/size*i+j] = B[N*i+j+offset];
 */
+  int offset = N/size*rank;
 
-  //cudaMemcpy(subA,a,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
-  //cudaMemcpy(subB,b,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
+  for (int i=0; i<N/size; i++)
+    for (int j=0; j<N; j++)
+      subA[N*i+j] = A[N*(i+0)+j];
+  for (int i=0; i<N; i++)
+    for (int j=0; j<N/size; j++)
+      subB[N/size*i+j] = B[N*i+j+offset];
+  cudaMemcpy(a,subA,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
+  cudaMemcpy(b,subB,N*N/size*sizeof(float),cudaMemcpyHostToDevice);
 
 
-
-
+  free(subA);
+  free(subB);
+  free(subC);
+  free(recv);
   cudaFree(a);
   cudaFree(b);
   cudaFree(c);
